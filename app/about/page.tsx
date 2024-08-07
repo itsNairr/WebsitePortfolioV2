6e38349d0bd949068ab6b49aa9dafc37cb94fc6c
Hari <coolhari2004@gmail.com>
2024-08-06 23:03:58 -0400

    Animations added to new about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,7 +20,7 @@ function page(prop: any) {
         </div>
         <h1 className="text-[40px] font-bold mb-10">About Me</h1>
         <div className="flex flex-row justify-evenly w-screen flex-wrap">
-          <div className="flex flex-col w-[40%] text-lg xs:w-full xs:text-base mb-5 xs:p-5">
+          <div className="flex flex-col w-[40%] text-lg xs:w-full xs:text-base mb-5 xs:p-5" data-aos="fade-right">
             <h1 className="text-[40px] font-bold xs:text-[30px] sm:text-[35px] mb-5">Education</h1>
             <h2 className="font-bold" >Queen's University</h2>
             <h3>BASc Mechatronics & Robotics Engineering</h3>
@@ -32,7 +32,7 @@ function page(prop: any) {
             <h3><span className="underline">Communications Coordinator:</span> Queen's Mechatronics and Robotics Discipline Club</h3>
           </div>
           <div className="w-[2px] bg-white sm:hidden xs:hidden"></div>
-          <div className="flex flex-col w-[40%] text-lg xs:w-full xs:p-5 xs:text-base">
+          <div className="flex flex-col w-[40%] text-lg xs:w-full xs:p-5 xs:text-base" data-aos="fade-left">
           <h1 className="text-[40px] font-bold xs:text-[30px] sm:text-[35px] mb-5">Internships & Employment</h1>
           <p>As I will be finishing my third year of university in the spring of 2025, I am actively seeking for <strong>8 - 16 month internship opportunities</strong> that will allow me to apply my academic knowledge and further develop my skills in engineering. With a strong foundation in programming and robotics, I am eager to contribute to a dynamic team and gain practical experience in the software and mechatronics industry.</p>
           </div>
