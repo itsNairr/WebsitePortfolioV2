39f17ac826d01bffc4f7e24a5fef03b3a881a95a
Hari <coolhari2004@gmail.com>
2025-04-24 17:00:24 -0400

    Updates as of May 2025

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -26,15 +26,14 @@ function page(prop: any) {
             <h3>BASc Mechatronics & Robotics Engineering</h3>
             <h3 className="mb-5">Graduation Year: 2026</h3>
             <h2 className="font-semibold" >Relevent Coursework</h2>
-            <h3 className="mb-5">Data Structures and Algorithms, Computer Architecture, Digital Systems, Mechatronics Design, Linear Algebra, Signals and Systems</h3>
+            <h3 className="mb-5">Data Structures and Algorithms, Computer Architecture, Digital Systems, Mechatronics Design, Linear Algebra, Signals and Systems, Introduction to Robotics, Numerical Methods, Computer Networks</h3>
             <h2 className="font-semibold" >Clubs and Societies</h2>
-            <h3><span className="underline">Co-founder and Vice-President:</span> Queen's University Malayalee Association</h3>
-            <h3><span className="underline">Communications Coordinator:</span> Queen's Mechatronics and Robotics Discipline Club</h3>
+            <h3><span className="underline">Co-founder:</span> Queen's University Malayalee Association</h3>
           </div>
           <div className="w-[2px] bg-white sm:hidden xs:hidden"></div>
           <div className="flex flex-col w-[40%] text-lg xs:w-full xs:p-5 xs:text-base" data-aos="fade-left">
           <h1 className="text-[40px] font-bold xs:text-[30px] sm:text-[35px] mb-5">Internships & Employment</h1>
-          <p>As I will be finishing my third year of university in the spring of 2025, I am actively seeking for <strong>8 - 16 month internship opportunities</strong> that will allow me to apply my academic knowledge and further develop my skills in engineering. With a strong foundation in programming and robotics, I am eager to contribute to a dynamic team and gain practical experience in the software and mechatronics industry.</p>
+          <p>As of May 2025, I am working at <strong> Scotiabank: Global Banking and Markets</strong> as a Trade Floor Technology Consultant for the next 16 months as part of my Professional Placement (QUIP).</p>
           </div>
         </div>
       </div>
