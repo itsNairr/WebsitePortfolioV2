4228f67df3919624a8f4dc4f6dd945565cff0b79
Hari Nair <coolhari2004@gmail.com>
2025-11-19 15:01:17 -0500

    Final touches

--- app/skills-projects/page.tsx
+++ app/skills-projects/page.tsx
@@ -21,6 +21,17 @@ import {
   SiPostgresql,
   SiFirebase,
   SiBun,
+  SiFlutter,
+  SiVuedotjs,
+  SiBlazor,
+  SiDotnet,
+  SiCsharp,
+  SiDart,
+  SiRos,
+  SiOpencv,
+  SiJira,
+  SiMicrosoftsqlserver,
+  SiConfluence,
 } from "react-icons/si";
 
 function page(prop: any) {
@@ -32,33 +43,40 @@ function page(prop: any) {
           <Project />
         </div>
         <h1 className="text-[40px] font-bold">Skills</h1>
-        <h1 className="text-[30px] mb-5">Languages</h1>
+        <h1 className="text-[30px] mb-5">Programming Languages</h1>
         <div className="flex flex-row flex-wrap text-[60px] sm:text-[50px] xs:text-[40px] gap-[50px] justify-evenly w-[90%]">
           <SiJavascript data-aos="zoom-in" className="iconshadow" />{" "}
           <SiTypescript data-aos="zoom-in" className="iconshadow" />{" "}
           <SiCplusplus data-aos="zoom-in" className="iconshadow" />{" "}
           <SiPython data-aos="zoom-in" className="iconshadow" />{" "}
           <SiHtml5 data-aos="zoom-in" className="iconshadow" />{" "}
-          <SiCss3 data-aos="zoom-in" className="iconshadow" />
+          <SiCss3 data-aos="zoom-in" className="iconshadow" />{" "}
+          <SiDart data-aos="zoom-in" className="iconshadow" />{" "}
+          <SiCsharp data-aos="zoom-in" className="iconshadow" />
         </div>
         <h1 className="text-[30px] my-5">Frameworks & Libraries</h1>
         <div className="flex flex-row flex-wrap text-[60px] sm:text-[50px] xs:text-[40px] gap-[50px] justify-evenly w-[90%]">
           <SiNextdotjs data-aos="zoom-in" className="iconshadow" />{" "}
           <SiReact data-aos="zoom-in" className="iconshadow" />{" "}
           <SiTailwindcss data-aos="zoom-in" className="iconshadow" />{" "}
-          <SiTensorflow data-aos="zoom-in" className="iconshadow" />{" "}
           <SiRedux data-aos="zoom-in" className="iconshadow" />{" "}
           <SiNodedotjs data-aos="zoom-in" className="iconshadow" />
-          <SiBun data-aos="zoom-in" className="iconshadow" />
-          <SiPandas data-aos="zoom-in" className="iconshadow" />
           <SiNumpy data-aos="zoom-in" className="iconshadow" />
+          <SiVuedotjs data-aos="zoom-in" className="iconshadow" />
+          <SiFlutter data-aos="zoom-in" className="iconshadow" />
+          <SiBlazor data-aos="zoom-in" className="iconshadow" />
+          <SiDotnet data-aos="zoom-in" className="iconshadow" />
+          <SiRos data-aos="zoom-in" className="iconshadow" />
+          <SiOpencv data-aos="zoom-in" className="iconshadow" />
         </div>
-        <h1 className="text-[30px] my-5">Databases</h1>
+        <h1 className="text-[30px] my-5">Databases & Tools</h1>
         <div className="flex flex-row flex-wrap text-[60px] sm:text-[50px] xs:text-[40px] gap-[50px] justify-evenly w-[90%]">
           {" "}
           <SiMongodb data-aos="zoom-in" className="iconshadow" />
-          <SiPostgresql data-aos="zoom-in" className="iconshadow" />
-          <SiFirebase data-aos="zoom-in" className="iconshadow" />
+          <SiMicrosoftsqlserver data-aos="zoom-in" className="iconshadow" />
+          <SiJira data-aos="zoom-in" className="iconshadow" />
+          <SiBun data-aos="zoom-in" className="iconshadow" />
+          <SiConfluence data-aos="zoom-in" className="iconshadow" />
         </div>
       </div>
     </div>
